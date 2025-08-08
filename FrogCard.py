1b709b20a6ee864f2bc81591ca65913b110fa4ae
honzhe05 <honzhe05@gmail.com>
2025-08-08 12:51:07 +0800

    waaaaaaaa

---
+++ FrogCard.py
@@ -0,0 +1,90 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
+from kivy.animation import Animation
+from kivy.uix.button import Button
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.image import Image
+from kivy.core.window import Window
+from kivy.core.text import LabelBase
+from kivy.uix.screenmanager import ScreenManager , Screen , NoTransition
+
+LabelBase.register(name='MyFont' , fn_regular = 'NotoSansTC-VariableFont_wght.ttf')
+Window.clearcolor = (0.66 , 0.36 , 0.17 , 0.6)
+
+class ImageButton(ButtonBehavior , Image):
+    pass
+    
+class StartScreen(Screen):
+    def __init__(self , **kwargs):
+        super().__init__(**kwargs)
+        layout = FloatLayout()
+        
+        title = Image(
+            source = 'Title.png' ,
+            size_hint = (None , None) ,
+            size = (1000 , 1000) ,
+            pos = (
+                (Window.width - 1000) / 2 , 
+                (Window.height - 850) * 0.75
+            ) ,
+            allow_stretch = True ,
+            keep_ratio = True
+        )
+        anim_up = Animation(y = title.y + 50 , duration = 0.5)
+        anim_down = Animation(y = title.y , duration = 0.8)
+        anim = anim_up + anim_down
+        anim.repeat = True
+        anim.start(title)
+        
+        layout.add_widget(title)
+        
+        self.startbtn = ImageButton(
+            source = 'StartButton.png' ,
+            size_hint = (None , None) ,
+            size = (500 , 500) ,
+            pos = (
+                (Window.width - 500) / 2 , 
+                (Window.height - 1000) * 0.25
+            ) ,
+            allow_stretch = True ,
+            keep_ratio = True
+        )
+        layout.add_widget(self.startbtn)
+        
+        self.startbtn.bind(on_release = self.go_to_game)
+        
+        self.add_widget (layout)
+        
+    def go_to_game(self, *args):
+        new_width = self.startbtn.width * 0.85
+        new_height = self.startbtn.height * 0.85
+        new_x = self.startbtn.x + (self.startbtn.width - new_width) / 2
+        new_y = self.startbtn.y + (self.startbtn.height - new_height) / 2
+
+        self.startbtn.size = (new_width, new_height)
+        self.startbtn.pos = (new_x, new_y)
+        self.manager.current = 'game'
+        
+class GameScreen(Screen):
+    def __init__(self , **kwargs):
+        super().__init__(**kwargs)
+        layout = FloatLayout()
+        self.add_widget(layout)
+        
+        layout.add_widget(
+            Label(
+                text = ' 空空如也\n¯⁠\⁠_⁠(⁠ツ⁠)⁠_⁠/⁠¯' ,
+                font_name = 'MyFont' ,
+                font_size = 100
+            )
+        )
+        
+class MyApp(App):
+    def build(self):
+        sm = ScreenManager(transition=NoTransition())
+        sm.add_widget(StartScreen(name= 'start'))
+        sm.add_widget(GameScreen(name= 'game'))
+        return sm
+        
+MyApp().run()
